21a8d62632f40d0a9e913a5eeca7248d23dadca0
kobake <kobake@users.sourceforge.net>
2014-11-17 23:37:11 +0900

    Show/hide fragments

--- src/com/example/myfragmentfade/MainActivity.java
+++ src/com/example/myfragmentfade/MainActivity.java
@@ -1,22 +1,98 @@
 package com.example.myfragmentfade;
 
+import android.animation.LayoutTransition;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.os.Build;
 
 public class MainActivity extends Activity {
 
+	public void buttonMethod1(View button){
+		// Fragment取得
+		Fragment f = getFragmentManager().findFragmentById(R.id.fragment1);
+
+		// Fragment操作の開始
+		FragmentTransaction ft = getFragmentManager().beginTransaction();
+		
+		// アニメーション設定
+		ft.setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out);
+
+		// Fragmentの表示または非表示
+		if(f.isVisible()){
+			ft.hide(f);
+		}
+		else{
+			ft.show(f);
+		}
+		
+		// 確定
+		ft.commit();
+	}
+
+	public void buttonMethod2(View button){
+		// Fragment取得
+		Fragment f = getFragmentManager().findFragmentById(R.id.fragment2);
+
+		// Fragment操作の開始
+		FragmentTransaction ft = getFragmentManager().beginTransaction();
+		
+		// アニメーション設定
+		ft.setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out);
+
+		// Fragmentの表示または非表示
+		if(f.isVisible()){
+			ft.hide(f);
+		}
+		else{
+			ft.show(f);
+		}
+		
+		// 確定
+		ft.commit();
+	}
+
+	// 実験：button0のshow/hide
+	public void buttonMethod3(View button){
+		View v = findViewById(R.id.button0);
+		LinearLayout container = (LinearLayout)findViewById(R.id.container);
+		if(v.getVisibility() == View.VISIBLE){
+			v.setVisibility(View.GONE);
+		}
+		else{
+			v.setVisibility(View.VISIBLE);
+		}
+	}
+	
+	LayoutTransition mTransition;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		// LayoutTransitionも付けておく？
+		LinearLayout container = (LinearLayout)findViewById(R.id.container);
+		mTransition = new LayoutTransition();
+		container.setLayoutTransition(mTransition);
+
+		/*
+		LinearLayout layout1 = (LinearLayout)findViewById(R.id.layout1);
+		layout1.setLayoutTransition(new LayoutTransition());
+
+		LinearLayout layout2 = (LinearLayout)findViewById(R.id.layout2);
+		layout2.setLayoutTransition(new LayoutTransition());
+		*/
+
+		
 		/*
 		if (savedInstanceState == null) {
 			getFragmentManager().beginTransaction()
